d4c5fe1e981afea4710d15f695314da079f71207
Erik Desjardins <erik.desjardins@gmail.com>
2019-08-05 00:52:56 -0400

    Small adjustments

--- lib/world.h
+++ lib/world.h
@@ -19,16 +19,16 @@ namespace GameOfLife
 {
     const int GAME_BASE_SIZE = 700;
     const int WORLD_SIZE = GAME_BASE_SIZE * MEM_MULTIPLIER;
-    const int FAMILY_COUNT = 10 * MEM_MULTIPLIER;
+    const int FAMILY_COUNT = 40 * MEM_MULTIPLIER;
     const int FIELDS_COUNT = 100 * MEM_MULTIPLIER;
-    const int WATER_PATCH_COUNT = 50 * MEM_MULTIPLIER;
+    const int WATER_PATCH_COUNT = 100 * MEM_MULTIPLIER;
     const int WATER_PATCH_SIZE = 1000 * MEM_MULTIPLIER;
 
-    const int FAMILY_MIN_SIZE = 50 * MEM_MULTIPLIER;
-    const int FAMILY_MAX_SIZE = 200 * MEM_MULTIPLIER;
+    const int FAMILY_MIN_SIZE = 10 * MEM_MULTIPLIER;
+    const int FAMILY_MAX_SIZE = 50 * MEM_MULTIPLIER;
 
-    const int FIELD_MIN_SIZE = 100 * MEM_MULTIPLIER;
-    const int FIELD_MAX_SIZE = 500 * MEM_MULTIPLIER;
+    const int FIELD_MIN_SIZE = 50 * MEM_MULTIPLIER;
+    const int FIELD_MAX_SIZE = 150 * MEM_MULTIPLIER;
 
     struct Tile
     {
